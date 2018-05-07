ee2dcfc5ae75a755432f0f5a35f20349cb317be5
Shogun <shogun@cowtech.it>
2018-05-07 11:44:04 +0200

    Treat NPM as Git when the package is private.

--- utils/environment.go
+++ utils/environment.go
@@ -6,10 +6,16 @@
 package utils
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"regexp"
 )
+
+type npmPackageJSON struct {
+	Private bool `json:"private"`
+}
 
 var versionMatcher = regexp.MustCompile("^(v(?:-?))")
 
@@ -27,7 +33,18 @@ func DetectRelease() int {
 	cwd, _ := os.Getwd()
 
 	if _, err := os.Stat(filepath.Join(cwd, "package.json")); !os.IsNotExist(err) {
-		return NpmRelease
+		// Parse the package.json
+		var parsed npmPackageJSON
+		rawConfiguration, err := ioutil.ReadFile(filepath.Join(cwd, "package.json"))
+
+		if err == nil {
+			err = json.Unmarshal(rawConfiguration, &parsed)
+		}
+
+		// If the package.json file is marked as private, treat as PlainRelease
+		if err != nil || !parsed.Private {
+			return NpmRelease
+		}
 	} else if specs, err := filepath.Glob(filepath.Join(cwd, "*.gemspec")); err == nil && len(specs) > 0 {
 		return GemRelease
 	}
